980ae648e90972f2872c636898af7b83466a47c5
Max Rafferty <maxprafferty@gmail.com>
2017-08-21 00:09:31 -0400

    horkin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from './components/grid.js';
 
 class App extends Component {
   render() {
@@ -10,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          Hello TownWorld
-        </p>
+        <div className="App-intro">
+            <Grid size={5} />
+        </div>
       </div>
     );
   }
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,9 +1,37 @@
+import React from 'react';
+let grid = [];
+
+const setupGrid = function (props) {
+    console.log('hork');
+    grid = ([...new Array(props.size)]).map(r => {
+        return (
+            <tr>
+                {([...new Array(props.size)]).map(c => {
+                  console.log('hork');
+                    return (
+                        <td>
+                            Hork
+                        </td>
+                    );
+                })}
+            </tr>
+        );
+    });
+    return grid;
+}
 
 const Grid = function (props) {
+    if (!grid.length) {
+        setupGrid(props);
+    }
 
     return (
         <div>
             Hello, Nerdos
+            <br />
+            <table>
+                {grid}
+            </table>
         </div>
     );
 }
