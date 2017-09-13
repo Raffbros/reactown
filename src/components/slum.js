c6b0eb5ae97c38dcffd8a6e9bb2a98b590fd8130
Luke Rafferty <luke.mingins.rafferty@gmail.com>
2017-09-13 13:46:11 -0400

    slumlet

---
+++ src/components/slum.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GenericBuilding from './generic.js'
+
+class Slum extends GenericBuilding {
+  constructor(props) {
+    super(props)
+    }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+export default Slum;
